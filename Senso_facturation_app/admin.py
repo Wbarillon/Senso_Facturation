b6044b9a8e63f69acd69aaccae7ba3d7d585f21d
emmduv <ehonig23@gmail.com>
2020-11-19 17:06:27 +0100

    Update

--- Senso_facturation_app/admin.py
+++ Senso_facturation_app/admin.py
@@ -5,22 +5,92 @@ from .models import *
 
 # Register your models here.
 
+
+@admin.register(Client)
+class ClientResource(ImportExportModelAdmin):
+    list_display = [
+        "id",
+        "nom_client",
+        "adresse_client",
+        "mail_client",
+        "telephone_client",
+    ]
+
+
+@admin.register(Facture)
+class FactureResource(ImportExportModelAdmin):
+    list_display = [
+        "id",
+        "emetteur",
+        "id_client",
+        "dernier_numero_facture_asso",
+        "dernier_numero_facture_senso",
+        "numero_facture",
+        "numero_commande",
+        "date_arrivee",
+        "date_depart",
+        "montant_paiement_arrhes",
+        "modes_paiement_arrhes",
+        "date_paiement_arrhes",
+        "montant_paiement_solde",
+        "modes_paiement_solde",
+        "date_paiement_solde",
+        "total",
+        "remarques",
+    ]
+
+
 @admin.register(Personne)
 class PersonneResource(ImportExportModelAdmin):
-    list_display = ['id', 'nom_personne', 'naissance_date', 'alimentation', 'medicament', 'remarques']
+    list_display = [
+        "id",
+        "nom_personne",
+        "prenom_personne",
+        "date_naissance",
+        "assujettie_taxe_sejour",
+        "alimentation",
+        "medicament",
+        "remarques",
+    ]
+
+
+@admin.register(Personne_Facture)
+class Personne_FactureResource(ImportExportModelAdmin):
+    list_display = [
+        "id",
+        "id_personne",
+        "id_facture",
+    ]
+
 
 @admin.register(Service_Produit)
 class Service_ProduitResource(ImportExportModelAdmin):
-    list_display = ['id', 'nom_service_produit', 'type_service_produit', 'prix']
+    list_display = [
+        "id",
+        "nom_service_produit",
+        "type_service_produit",
+        "prix_unitaire",
+    ]
+
+
+@admin.register(Service_Produit_Commande)
+class Service_Produit_CommandeResource(ImportExportModelAdmin):
+    list_display = [
+        "id",
+        "id_service_produit",
+        "id_facture",
+        "quantite",
+        "prix_total_ht",
+        "remise",
+        "date_commande",
+    ]
+
 
 @admin.register(Taxe)
 class TaxeResource(ImportExportModelAdmin):
-    list_display = ['id', 'nom_taxe', 'taux']
+    list_display = ["id", "nom_taxe", "initiales", "taux", "mini", "montant_fixe"]
 
-@admin.register(Commande)
-class CommandeResource(ImportExportModelAdmin):
-    list_display = ['id', 'id_personne', 'id_service_produit', 'arrhes', 'commande_date']
 
-@admin.register(Sejour)
-class SejourResource(ImportExportModelAdmin):
-    list_display = ['id', 'id_commande', 'arrivee_date', 'depart_date']
+@admin.register(Taxe_Service_Produit)
+class Taxe_Service_ProduitResource(ImportExportModelAdmin):
+    list_display = ["id", "id_service_produit", "id_taxe"]
--- Senso_facturation_app/models.py
+++ Senso_facturation_app/models.py
@@ -82,182 +82,11 @@ class Client(models.Model):
         verbose_name_plural = "Clients"
 
 
-class Personne(models.Model):
-    id = models.AutoField(primary_key=True)
-    nom_personne = models.CharField(
-        verbose_name="Nom", max_length=50, null=True, blank=True
-    )
-    prenom_personne = models.CharField(
-        verbose_name="Prénom", max_length=50, null=True, blank=True
-    )
-    date_naissance = models.DateField(
-        verbose_name="Date de naissance", null=True, blank=True
-    )
-    assujettie_taxe_sejour = models.BooleanField(
-        verbose_name="Assujettie à la taxe de séjour", null=True, blank=True
-    )
-    alimentation = models.TextField(verbose_name="Alimentation", null=True, blank=True)
-    medicament = models.TextField(verbose_name="Médicament", null=True, blank=True)
-    remarques = models.TextField(verbose_name="Remarques", null=True, blank=True)
-
-    def __str__(self):
-        return self.nom_personne
-
-    class Meta:
-        verbose_name = "Personne"
-        verbose_name_plural = "Personnes"
-
-
-class Personne_Facture(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_personne = models.ForeignKey(
-        "Personne",
-        on_delete=models.PROTECT,
-        verbose_name="Identifiant Personne",
-        related_name="facture_personne",
-        null=True,
-        blank=True,
-    )
-    id_facture = models.ForeignKey(
-        "Facture",
-        on_delete=models.PROTECT,
-        verbose_name="Identifiant Facture",
-        related_name="personne_facture",
-        null=True,
-        blank=True,
-    )
-
-
-class Service_Produit(models.Model):
-    id = models.AutoField(primary_key=True)
-    nom_service_produit = models.CharField(
-        verbose_name="Nom", max_length=50, null=True, blank=True
-    )
-    type_service_produit = models.CharField(
-        verbose_name="Type", max_length=50, null=True, blank=True
-    )
-    # On teste comme ça, si il y a des problèmes d'arrondis, switcher avec un type Float
-    prix_unitaire = models.DecimalField(
-        verbose_name="Prix Unitaire",
-        max_digits=8,
-        decimal_places=2,
-        null=True,
-        blank=True,
-    )
-
-    def __str__(self):
-        return self.nom_service_produit
-
-    class Meta:
-        verbose_name = "Service/Produit"
-        verbose_name_plural = "Services/Produits"
-
-
-class Service_Produit_Commande(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_service_produit = models.ForeignKey(
-        "Service_Produit",
-        on_delete=models.PROTECT,
-        verbose_name="Service commandé",
-        related_name="service_produit_commande",
-        null=True,
-        blank=True,
-    )
-    id_facture = models.ForeignKey(
-        "Facture",
-        on_delete=models.PROTECT,
-        verbose_name="Facture",
-        related_name="service_produit_commande",
-        null=True,
-        blank=True,
-    )
-    quantite = models.IntegerField(verbose_name="Quantité", null=True, blank=True)
-    # On teste comme ça, si il y a des problèmes d'arrondis, switcher avec un type Float
-    prix_total_ht = models.DecimalField(
-        verbose_name="Prix Total HT",
-        max_digits=8,
-        decimal_places=2,
-        null=True,
-        blank=True,
-    )
-    remise = models.DecimalField(
-        verbose_name="Remise", max_digits=8, decimal_places=2, null=True, blank=True
-    )
-    arrhes = models.DecimalField(
-        verbose_name="Arrhes", max_digits=8, decimal_places=2, null=True, blank=True
-    )
-    date_commande = models.DateField(
-        verbose_name="Date de commande", null=True, blank=True
-    )
-
-    class Meta:
-        verbose_name = "Service/Produit commandé"
-        verbose_name_plural = "Services/Produits commandés"
-
-
-class Taxe(models.Model):
-    id = models.AutoField(primary_key=True)
-    nom_taxe = models.CharField(
-        verbose_name="Nom", max_length=50, null=True, blank=True
-    )
-    initiales = models.CharField(
-        verbose_name="Initiales", max_length=10, null=True, blank=True
-    )
-    taux = models.DecimalField(
-        verbose_name="Taux", max_digits=4, decimal_places=2, null=True, blank=True
-    )
-    mini = models.DecimalField(
-        verbose_name="Minimal", max_digits=4, decimal_places=2, null=True, blank=True
-    )
-    montant_fixe = models.DecimalField(
-        verbose_name="Montant", max_digits=4, decimal_places=2, null=True, blank=True
-    )
-
-
-class Taxe_Service_Produit(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_service_produit = models.ForeignKey(
-        "Service_Produit",
-        on_delete=models.PROTECT,
-        verbose_name="Service/produit",
-        related_name="taxe_service_produit",
-        null=True,
-        blank=True,
-    )
-    id_taxe = models.ForeignKey(
-        "Taxe",
-        on_delete=models.PROTECT,
-        verbose_name="Taxe Service/Produit",
-        related_name="taxe_service_produit",
-        null=True,
-        blank=True,
-    )
-
-
-class Taxe_Service_Produit_Commande(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_service_produit_commande = models.ForeignKey(
-        "Service_Produit_Commande",
-        on_delete=models.PROTECT,
-        verbose_name="Service/produit commandé",
-        related_name="taxe_service_produit_commande",
-        null=True,
-        blank=True,
-    )
-    id_taxe = models.ForeignKey(
-        "Taxe",
-        on_delete=models.PROTECT,
-        verbose_name="Taxe Service/Produit Commandé",
-        related_name="taxe_service_produit_commande",
-        null=True,
-        blank=True,
-    )
-
-
 class Facture(models.Model):
     id = models.AutoField(primary_key=True)
     emetteur = models.CharField(
         verbose_name="Emetteur de la facture",
+        max_length=20,
         choices=[("", ""), ("Asso", "Association"), ("Senso", "Sensoryalis")],
         default="",
     )
@@ -265,7 +94,7 @@ class Facture(models.Model):
         "Client",
         on_delete=models.PROTECT,
         verbose_name="Client",
-        related_name="facture",
+        related_name="factures",
         null=True,
         blank=True,
     )
@@ -287,12 +116,10 @@ class Facture(models.Model):
     numero_commande = models.CharField(
         verbose_name="Numéro de la commande", max_length=50, null=True, blank=True
     )
-     date_arrivee = models.DateField(
+    date_arrivee = models.DateField(
         verbose_name="Date d'arrivée", null=True, blank=True
     )
-    date_depart = models.DateField(
-        verbose_name="Date de départ", null=True, blank=True
-    )
+    date_depart = models.DateField(verbose_name="Date de départ", null=True, blank=True)
     montant_paiement_arrhes = models.DecimalField(
         verbose_name="Montant des arrhes",
         max_digits=8,
@@ -340,3 +167,164 @@ class Facture(models.Model):
     class Meta:
         verbose_name = "Facture"
         verbose_name_plural = "Factures"
+
+
+class Personne(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom_personne = models.CharField(
+        verbose_name="Nom", max_length=50, null=True, blank=True
+    )
+    prenom_personne = models.CharField(
+        verbose_name="Prénom", max_length=50, null=True, blank=True
+    )
+    date_naissance = models.DateField(
+        verbose_name="Date de naissance", null=True, blank=True
+    )
+    assujettie_taxe_sejour = models.BooleanField(
+        verbose_name="Assujettie à la taxe de séjour", null=True, blank=True
+    )
+    alimentation = models.TextField(verbose_name="Alimentation", null=True, blank=True)
+    medicament = models.TextField(verbose_name="Médicament", null=True, blank=True)
+    remarques = models.TextField(verbose_name="Remarques", null=True, blank=True)
+
+    def __str__(self):
+        return self.prenom_personne + " " + self.nom_personne
+
+    class Meta:
+        verbose_name = "Personne"
+        verbose_name_plural = "Personnes"
+
+
+class Personne_Facture(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_personne = models.ForeignKey(
+        "Personne",
+        on_delete=models.PROTECT,
+        verbose_name="Identifiant Personne",
+        related_name="personnes",
+        null=True,
+        blank=True,
+    )
+    id_facture = models.ForeignKey(
+        "Facture",
+        on_delete=models.PROTECT,
+        verbose_name="Identifiant Facture",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Personne/Facture"
+        verbose_name_plural = "Personnes/Factures"
+
+
+class Service_Produit(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom_service_produit = models.CharField(
+        verbose_name="Nom", max_length=50, null=True, blank=True
+    )
+    type_service_produit = models.CharField(
+        verbose_name="Type", max_length=50, null=True, blank=True
+    )
+    # On teste comme ça, si il y a des problèmes d'arrondis, switcher avec un type Float
+    prix_unitaire = models.DecimalField(
+        verbose_name="Prix Unitaire",
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.nom_service_produit
+
+    class Meta:
+        verbose_name = "Service/Produit"
+        verbose_name_plural = "Services/Produits"
+
+
+class Service_Produit_Commande(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_service_produit = models.ForeignKey(
+        "Service_Produit",
+        on_delete=models.PROTECT,
+        verbose_name="Service commandé",
+        related_name="services",
+        null=True,
+        blank=True,
+    )
+    id_facture = models.ForeignKey(
+        "Facture",
+        on_delete=models.PROTECT,
+        verbose_name="Facture",
+        null=True,
+        blank=True,
+    )
+    quantite = models.IntegerField(verbose_name="Quantité", null=True, blank=True)
+    # On teste comme ça, si il y a des problèmes d'arrondis, switcher avec un type Float
+    prix_total_ht = models.DecimalField(
+        verbose_name="Prix Total HT",
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    remise = models.DecimalField(
+        verbose_name="Remise", max_digits=8, decimal_places=2, null=True, blank=True
+    )
+    date_commande = models.DateField(
+        verbose_name="Date de commande", null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = "Service/Produit commandé"
+        verbose_name_plural = "Services/Produits commandés"
+
+
+class Taxe(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom_taxe = models.CharField(
+        verbose_name="Nom", max_length=50, null=True, blank=True
+    )
+    initiales = models.CharField(
+        verbose_name="Initiales", max_length=10, null=True, blank=True
+    )
+    taux = models.DecimalField(
+        verbose_name="Taux", max_digits=4, decimal_places=2, null=True, blank=True
+    )
+    mini = models.DecimalField(
+        verbose_name="Minimal", max_digits=4, decimal_places=2, null=True, blank=True
+    )
+    montant_fixe = models.DecimalField(
+        verbose_name="Montant", max_digits=4, decimal_places=2, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.nom_taxe
+
+    class Meta:
+        verbose_name = "Taxe"
+        verbose_name_plural = "Taxes"
+
+
+class Taxe_Service_Produit(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_service_produit = models.ForeignKey(
+        "Service_Produit",
+        on_delete=models.PROTECT,
+        verbose_name="Service/produit",
+        related_name="taxes",
+        null=True,
+        blank=True,
+    )
+    id_taxe = models.ForeignKey(
+        "Taxe",
+        on_delete=models.PROTECT,
+        verbose_name="Taxe Service/Produit",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Taxe/Service/produit"
+        verbose_name_plural = "Taxes/Services/Produits"
